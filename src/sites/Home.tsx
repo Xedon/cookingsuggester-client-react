ae8bd8e20afb3833e8ee2119551ed69ad4f75c36
Emanuel Vollmer <xedon@xedon-coding.de>
2019-10-03 22:27:21 +0200

    Add Home

--- src/sites/Home.tsx
+++ src/sites/Home.tsx
@@ -1,9 +1,19 @@
 import * as React from "react";
+import {Trans} from "react-i18next";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-export class Home extends React.Component<any,any>{
+export class Home extends React.Component<any, any> {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
-            <span></span>
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        <Trans i18nKey="home.description"></Trans>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
